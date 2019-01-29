9f1d575d78a845f13ec4b2573fcbd6814d78f070
shashankk <shashankk@192.168.0.102>
2019-01-30 01:08:49 +0530

    Updated Version

--- LiveInBliss/src/main/java/controller/RegistrationServlet.java
+++ LiveInBliss/src/main/java/controller/RegistrationServlet.java
@@ -22,23 +22,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import Model.UserData;
-
-
-
-
-
-
-
-
-
+import email.Test;
 
 @WebServlet({"/RegistrationServlet"})
-public class RegistrationServlet
-  extends HttpServlet
+public class RegistrationServlet extends HttpServlet
 {
   private static final long serialVersionUID = 1L;
   
   public RegistrationServlet() {}
+  
+  UserData ud = new UserData();
   
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
@@ -49,107 +42,105 @@ public class RegistrationServlet
   {
     PrintWriter out = response.getWriter();
     
-    String first_name = request.getParameter("f1");
-    String last_name = request.getParameter("l1");
-    String phone = request.getParameter("p1");
-    String email = request.getParameter("e1");
-    String uname = request.getParameter("uname");
-    String user_dob = request.getParameter("udate");
-    String password = request.getParameter("pwd");
+   
     
-    String to = "svchaturvedi9@gmail.com";
-    String from = "svchaturvedi9@gmail.com";
-    String host = "localhost";
     
-    UserData ud = new UserData();
     
-    ud.setFirst_name(first_name);
-    ud.setLast_name(last_name);
-    ud.setUname(uname);
-    ud.setPhone(phone);
-    ud.setEmail(email);
-    ud.setUser_dob(user_dob);
-    ud.setPassword(password);
+    ud.setFirst_name(request.getParameter("f1"));
+    ud.setLast_name(request.getParameter("l1"));
+    ud.setUname(request.getParameter("uname"));
+    ud.setPhone(request.getParameter("p1"));
+    ud.setEmail(request.getParameter("e1"));
+    ud.setUser_dob(request.getParameter("udate"));
+    ud.setPassword(request.getParameter("pwd"));
+    ud.setConfirm_password(request.getParameter("cpwd"));
     
 
-    if ((first_name.isEmpty()) || (last_name.isEmpty()) || (phone.isEmpty()) || (email.isEmpty()) || (password.isEmpty())) {
-      RequestDispatcher rd = request.getRequestDispatcher("Register.jsp");
-      out.println("<font color=red>Please fill all the fields</font>");
-      rd.include(request, response);
-    }
-    else
-    {
-      try {
-        Class.forName("com.mysql.jdbc.Driver");
-      }
-      catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      }
+    if (ud.getConfirm_password().equals(ud.getPassword())) {
       
+    	System.out.println("Sending email");
+    	writeData();
+    	Test t=new Test(ud.getEmail(), "Live In Bliss", "Welcome to Live In Bliss!!! We are happy that you have become a member of Live In Bliss. You will get customized offers from us during festive seasons. So get ready for some exciting deals only from Live In Bliss-Aromatize your Atmosphere");
+    	try 
+        {
+            t.sendEmail();
+        } 
+    	
+        catch (Exception ex) 
+        {
+            out.println(ex.getMessage());
+        }
+    	System.out.println("Email Sent");
 
-
-      try
-      {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/liveinbliss?autoReconnect=true&useSSL=false", "root", "Mohini24");
-        
-
-        String query = "insert into users values(?,?,?,?,?,?,?)";
-        
-        PreparedStatement ps = con.prepareStatement(query);
-        
-
-
-        ps.setString(1, first_name);
-        
-
-        ps.setString(2, last_name);
-        
-
-        ps.setString(3, uname);
-        
-
-        ps.setString(4, phone);
-        
-
-        ps.setString(5, email);
-        
-
-        ps.setString(6, user_dob);
-        
-
-        ps.setString(7, password);
-        
-
-
-        ps.executeUpdate();
-        
-        System.out.println("successfuly inserted");
-        
-
-
-        ps.close();
-        
-
-
-
-        con.close();
-
-
-      }
-      catch (SQLException e)
-      {
-
-        e.printStackTrace();
-      }
+     
       HttpSession session = request.getSession();
-      session.setAttribute("username", first_name);
+      session.setAttribute("username", ud.getUname());
       response.sendRedirect("Success.jsp");
       
-
-
-
-
       
     }
+    
+    
+    else {
+  	  response.sendRedirect("Register.jsp");
+    }
   }
+  
+  
+  public void writeData() {
+	  
+	  try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	
+	  
+	  Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/liveinbliss?autoReconnect=true&useSSL=FALSE", "root", "Mohini24");
+      
+
+      String query = "insert into users values(?,?,?,?,?,?,?)";
+      
+      PreparedStatement ps = con.prepareStatement(query);
+      
+
+
+      ps.setString(1, ud.getFirst_name());
+      
+
+      ps.setString(2, ud.getLast_name());
+      
+
+      ps.setString(3, ud.getUname());
+      
+
+      ps.setString(4, ud.getPhone());
+      
+
+      ps.setString(5, ud.getEmail());
+      
+
+      ps.setString(6, ud.getUser_dob());
+      
+
+      ps.setString(7, ud.getPassword());
+      
+
+
+      ps.executeUpdate();
+      
+      
+	  } catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+      
+	  
+	  
+	  
+	  
+  }
+  
+  
+  
+  
+  
+  
 }
---
+++ LiveInBliss/src/main/java/email/Test.java
@@ -0,0 +1,62 @@
+package email;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Test 
+{
+   
+	   
+	    String to;
+	    String subject;
+	    String text;
+
+	    public Test(String to, String subject, String text) {
+	        this.to = to;
+	        this.subject = subject;
+	        this.text = text;
+	    }
+
+	    
+
+	    public void sendEmail() throws Exception
+	    {
+	          System.out.println("=============mail===========");
+			final String username = "svchaturvedi9@gmail.com";
+			final String password = "Mohini99";
+
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
+
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(username));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+				message.setSubject(subject);
+				message.setText(text);
+
+				Transport.send(message);
+
+				System.out.println("Done");
+
+			
+	    }
+	   
+	}
+
+
